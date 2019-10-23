d1b66155b03ee1e86ad5419c648043c19e03878f
JiaXIng <s3a632055@gm.student.ncut.edu.tw>
2019-10-23 11:17:19 +0800

    每頁標題就是頁面名稱

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel 關於本站</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <font color=#00008b>Laravel 關於本站</font>
+                    <font color=#00008b face="標楷體">Laravel 關於本站</font>
                 </div>
 
                 <div class="links">
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel 最新消息</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <font color=#228b22>Laravel 最新消息</font>
+                    <font color=#228b22 face="標楷體">Laravel 最新消息</font>
                 </div>
 
                 <div class="links">
